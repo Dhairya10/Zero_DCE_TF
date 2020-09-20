d55f4afdf01c28cee97d9a91f3a406fe2e7319cb
Dhairya Kumar <dhairya.kumar@wobot.ai>
2020-09-20 08:50:37 +0530

    Modified train.py

--- train.py
+++ train.py
@@ -118,12 +118,12 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 
 	# Input Parameters
-	parser.add_argument('--lowlight_images_path', type=str, default="/home/inhand/Tu/DCE/Dataset_Part1/")
+	parser.add_argument('--lowlight_images_path', type=str, default="/home/ubuntu/Zero_DCE_TF/dataset/")
 	parser.add_argument('--lr', type=float, default=0.0001)
 	parser.add_argument('--gpu', type=int, default=0)
 	parser.add_argument('--grad_clip_norm', type=float, default=0.1)
 	parser.add_argument('--num_epochs', type=int, default=200)
-	parser.add_argument('--train_batch_size', type=int, default=8)
+	parser.add_argument('--train_batch_size', type=int, default=16)
 	parser.add_argument('--val_batch_size', type=int, default=4)
 	parser.add_argument('--num_workers', type=int, default=4)
 	parser.add_argument('--display_iter', type=int, default=2)
